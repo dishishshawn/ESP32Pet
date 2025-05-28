125506b52c3a7ed090756bc816c1f566d0cebd66
dishishshawn <shawn.a.agarwal-1@ou.edu>
2025-05-28 12:03:16 -0500

    Added scroll menu library

---
+++ src/scroll_menu.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include "scroll_menu.h"
+
+ScrollMenu::ScrollMenu(int x, int y, int menuSize) {
+    this->menuSize = menuSize - 1;
+    this->x = x;
+    this->y = y;
+}
+
+void ScrollMenu::moveRight(){
+    if (nextSection == menuSize) {
+        nextSection = 0;
+        activeSection ++;
+        prevSection ++;
+    } else if (activeSection == menuSize) {
+        activeSection = 0;
+        nextSection ++;
+        prevSection ++;
+    } else if (prevSection == menuSize) {
+        prevSection = 0;
+        activeSection ++;
+        nextSection ++;
+    } else {
+        activeSection ++;
+        nextSection ++;
+        prevSection ++;
+    }
+}
+
+void ScrollMenu::moveLeft(){
+    if (prevSection == 0) {
+        prevSection = menuSize;
+        activeSection --;
+        nextSection --;
+    } else if (activeSection == 0) {
+        activeSection = menuSize;
+        prevSection --;
+        nextSection --;
+    } else if (nextSection == 0) {
+        nextSection = menuSize;
+        activeSection --;
+        prevSection --;
+    } else {
+        activeSection --;
+        prevSection --;
+        nextSection --;
+    }
+}
---
+++ src/scroll_menu.h
@@ -0,0 +1,26 @@
+/*
+    Scroll_menu.h - Library for Scroll Menu instance.
+    Created by Shawn Agarwal, May 2025
+    This library is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+*/
+
+#ifndef SCROLL_MENU_H
+#define SCROLL_MENU_H
+#include <Arduino.h>
+
+class ScrollMenu {
+    public:
+    ScrollMenu(int x, int y, int menuSize);
+
+    int x;
+    int y;
+    int menuSize;
+    int prevSection = 0;
+    int activeSection = 1;
+    int nextSection = 2;
+
+    void moveLeft();
+    void moveRight();
+};
+
+#endif
